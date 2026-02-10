f2c4337111c18eb7d6b213a8fc72bdea2e20de81
Choco-Gitbo <night.of.onion@gmail.com>
2026-02-10 09:58:41 +0900

    Add files via upload

---
+++ chart.js
@@ -0,0 +1,18 @@
+new Chart(ctx, {
+  type: "radar",
+  data,
+  options: {
+    responsive: true,
+    maintainAspectRatio: false, // ← これ必須
+    scales: {
+      r: {
+        min: 0,
+        max: 500,
+        ticks: { display: false }
+      }
+    },
+    plugins: {
+      legend: { display: false }
+    }
+  }
+});
