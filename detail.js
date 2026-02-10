ef2c270892946af6c25391b2a5e6679bf07d6403
Choco-Gitbo <night.of.onion@gmail.com>
2026-02-10 13:10:11 +0900

    Add files via upload

--- detail.js
+++ detail.js
@@ -212,24 +212,6 @@ function drawHexChart(b) {
   const canvas = document.getElementById("radarChart");
   const ctx = canvas.getContext("2d");
 
-  new Chart(ctx, {
-    type: "radar",
-    data,
-    options: {
-      responsive: true,
-      maintainAspectRatio: false, // ← これ必須
-      scales: {
-        r: {
-          min: 0,
-          max: 500,
-          ticks: { display: false }
-        }
-      },
-      plugins: {
-        legend: { display: false }
-      }
-    }
-  });  
 
   ctx.clearRect(0,0,canvas.width,canvas.height);
 
