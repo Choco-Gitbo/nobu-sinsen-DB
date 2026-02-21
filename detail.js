f81c62e093b31016738e15cb497e5b830f177bd3
Choco-Gitbo <night.of.onion@gmail.com>
2026-02-21 17:58:04 +0900

    Add files via upload

--- detail.js
+++ detail.js
@@ -141,7 +141,7 @@ function createSenpoCard(senpo, states, label) {
 
   const descEl = document.createElement("div");
   descEl.className = "senpo-desc";
-  descEl.textContent = senpo.description.split("|").join("<br>");
+  descEl.innerHTML = senpo.description.split("|").join("<br>");
   descEl.style.display = "none";  
 
   titleRow.addEventListener("click", () => {
