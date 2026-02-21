3e292a30299d21d33ab24106cae937034f0137a2
Choco-Gitbo <night.of.onion@gmail.com>
2026-02-21 11:47:30 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -140,7 +140,7 @@ fetch("data/senpo.csv")
 
 function renderSenpoList(data){
 
-  const list = document.getElementById("senpoList");
+  const list = document.getElementById("senpo-List");
   list.innerHTML = "";
 
   data.forEach(s=>{
