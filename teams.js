12be9144e81ef08efef809a3cdcfac226d6fdb3b
Choco-Gitbo <night.of.onion@gmail.com>
2026-03-07 21:25:30 +0900

    Add files via upload

--- teams.js
+++ teams.js
@@ -76,7 +76,7 @@ function setBushoData(column,id){
   const attrNodes=column.querySelectorAll(".attr-grid div")
 
   attrs.forEach((a,i)=>{
-    const Lv=column.querySelector(".Lv")
+    const Lv= parseInt(column.querySelector(".Lv"))
     const num1 = parseFloat(attrb[i] || 0)
     const num2 = parseFloat(attrg[i] || 0)
     const num = num1 + (num2 * (Lv -1))
