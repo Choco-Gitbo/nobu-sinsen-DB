8a49e53a998ebfc9edba41b1ff094e87182c688b
Choco-Gitbo <night.of.onion@gmail.com>
2026-03-10 20:30:21 +0900

    Add files via upload

--- teams.js
+++ teams.js
@@ -85,7 +85,7 @@ function refreshBushoSelect(){
     const current = select.value
     select.innerHTML = `<option value="">武将選択</option>` 
     getFilteredBusho().forEach(b=>{
-      /* if(selected.includes(b.id) && b.id !== current) return */
+      if(!matchBushoFilter(b) && b.id !== current) return 
       const op=document.createElement("option")
       op.value=b.id
       op.textContent=b.name
@@ -94,8 +94,8 @@ function refreshBushoSelect(){
 
     })
     select.value = current
-    select.textContent = DB.busho[current].name 
     
+
   })
 }
 
