513ae48a48c4664cf6ccc78b49deecb42161da7d
Choco-Gitbo <night.of.onion@gmail.com>
2026-03-14 09:07:05 +0900

    Add files via upload

--- teams.js
+++ teams.js
@@ -456,6 +456,11 @@ function createSenpoStateFilter(){
 
 function setupTeamType(){
 
+  document.querySelectorAll(".team-grid").innerHTML =`
+  兵種：<select class="unit-select"></select>　<div class="nowcost">0</div><div > / </div>
+    <select class="maxcost"></select>
+  `
+  
   const types = ["騎兵","弓兵","鉄砲","足軽","兵器"]
   document.querySelectorAll(".unit-select").forEach(select=>{
     select.innerHTML = ""
