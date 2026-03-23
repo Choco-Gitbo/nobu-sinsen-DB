a9575d236c038c30dfb1bbfa8781298c88beefeb
Choco-Gitbo <night.of.onion@gmail.com>
2026-03-23 12:54:39 +0900

    Add files via upload

--- teams1.js
+++ teams1.js
@@ -467,7 +467,7 @@ document.querySelectorAll(".heigaku-type").forEach(select=>{
 /* チェンジイベント処理 */
 document.addEventListener("change",e=>{
     /* 武将選択の変更処理 */
-  if(e.target.classList.contains("busho-name")){
+  if(e.target.classList.contains("filter-b")){
     createBushoSelect()
   }
 
