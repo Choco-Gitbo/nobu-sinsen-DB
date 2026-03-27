e27b3de3d082642fdfed9068578e9e82dc67e372
Choco-Gitbo <night.of.onion@gmail.com>
2026-03-27 15:50:32 +0900

    Add files via upload

--- teams1.js
+++ teams1.js
@@ -639,7 +639,6 @@ function makeTable(){
               <span>C0</span>
               <input class="chain-chk" type="checkbox" checked="false" defaultchecked="true" />
             </td>
-          
           <td class="cost">C0</td>
           <td rowspan="2">
             <select class="select busho-name">
@@ -687,7 +686,7 @@ function makeTable(){
           </td>
         </tr>
         <tr class="unit-row-2" data-group="${i}">
-          <td></td>
+          <td>''</td>
           <td class="rank">R0</td>
           <td class="heigaku">正</td>
           <td class="heigaku-sei2">
