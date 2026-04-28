e5a811bd0168d0fc902190c142caa1d70105d8c5
Choco-Gitbo <night.of.onion@gmail.com>
2026-04-28 16:10:18 +0900

    Add files via upload

--- BS_BattleField.js
+++ BS_BattleField.js
@@ -759,7 +759,7 @@ export class BattleField {
             this.#processTurnEnd();
         }
 
-        this.add_log("=== 戦��終了 ===");
+        this.add_log("=== 戦闘終了 ===");
         this.show_result();
     }
 
@@ -953,7 +953,7 @@ export class BattleField {
                 this.add_log(`  ${busho.colored_name} は【${skill.colored_name}】の発動率変更（${skill.rate}％ -> ${sRate}％)`);
             }
 
-            if (Math.random()  <= (sRate /100) || isPrep) {
+            if ((Math.random()  <= (sRate /100)) || isPrep) {
                 if (!isPrep) {
                     const prepState = {
                         name: `準備:${skill.name}`,
