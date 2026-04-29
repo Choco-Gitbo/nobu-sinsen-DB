f24f5f4f04ec6a36ed51b5c2455779ed46e3f559
Choco-Gitbo <night.of.onion@gmail.com>
2026-04-30 07:34:02 +0900

    Add files via upload

--- BS_main.js
+++ BS_main.js
@@ -66,7 +66,7 @@ async function runMultipleBattles(count) {
         // 武将ごとのダメージ集計（ここで最大・最小・合計を更新）
         report.armyA.forEach(b => {
             if (!summary.details[b.name]) {
-                summary.details[b.name] = { dmgSum: 0, dmgMax: 0, dmgMin: Infinity};  
+                summary.details[b.name] = { dmgSum: 0, dmgMax: 0, dmgMin: Infinity,skills:{}};  
             }
             const sd = summary.teamDamage;
             sd.sum += b.damage;
@@ -85,7 +85,7 @@ async function runMultipleBattles(count) {
 
             b.skill_details.forEach(ss => {
                 summary.details[b.name].skills[ss.name]={dmg:{sum:0,max:0,min:Infinity},
-                    heal:{sum:0,max:0,min:Infinity},count:{sum:0,max:0,min:Infinity}}
+                    heal:{sum:0,max:0,min:Infinity},count:{sum:0,max:0,min:Infinity}};
                 let ss1 = summary.details[b.name].skills[ss.name]
                 ss1.count.sum += ss.count
                 ss1.count.max = Math.max(ss1.count.max, ss.count);
