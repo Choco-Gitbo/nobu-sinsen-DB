75e6c49f260e4d52b597848a9a93a8ed6f5f2516
Choco-Gitbo <night.of.onion@gmail.com>
2026-04-29 22:34:21 +0900

    Add files via upload

--- BS_main.js
+++ BS_main.js
@@ -83,6 +83,12 @@ async function runMultipleBattles(count) {
             sh.max = Math.max(sh.max, b.heal);
             sh.min = Math.min(sh.min, b.heal);
 
+            [b.name].skills.forEach(ss => {
+                let ss1 = summary.details[b.name].skills[ss.name]
+                ss1.cntsum += ss.count
+                ss1.max = Math.max(ss1.max, ss.count);
+                ss1.min = Math.min(ss1.min, ss.count);
+            })
         });
 
         if (i == (count-1)){
